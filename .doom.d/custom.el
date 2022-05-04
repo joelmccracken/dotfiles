4ace5a9ef05fb94fa841dc5ae1b747e6d8d33881
Joel McCracken <mccracken.joel@gmail.com>
2022-05-04 00:43:31 -0400

    udpates

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" default))
  '(safe-local-variable-values
    '((eval progn
            (make-local-variable 'projectile-make-test-cmd)
