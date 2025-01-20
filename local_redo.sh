1eca7d9095f424cd8dcb692036d8708ba6766d93
Joel McCracken <mccracken.joel@gmail.com>
2025-01-19 20:08:10 -0500

    stuff

--- local_redo.sh
+++ local_redo.sh
@@ -15,4 +15,17 @@ rm -rf ~/.config/home-manager; nix profile remove home-manager-path; nix-collect
 nix build --no-link .#homeConfigurations.angrist."joel.mccracken".activationPackage --show-trace
 "$(nix path-info ~/workstation/#homeConfigurations.${WORKSTATION_NAME}.$(whoami).activationPackage)"/activate --show-trace
 
+WORKSTATION_HOME_MANAGER_VERSION=master
 nix run "home-manager/$WORKSTATION_HOME_MANAGER_VERSION" -- switch
+nix run -L --debug "home-manager/master" -- switch
+
+
+
+nix run -v -L .#homeConfigurations."joel.mccracken".activationPackage --show-trace
+
+nix-repl> :p outputs.homeConfigurations."joel.mccracken".activationPackage
+
+
+
+
+nix run -v -L ".#homeConfigurations.\"joel.mccracken\".activationPackage" --show-trace
