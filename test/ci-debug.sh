8d7b66039c5a44c40b9c8d1ff0df42f494a8bb3b
Joel McCracken <mccracken.joel@gmail.com>
2025-04-05 22:33:43 -0400

    next stage in CI

--- test/ci-debug.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-env | sort
-
-jq '.' <<-EOF
-  $GITHUB_JSON_PAYLOAD
-EOF
---
+++ test/ci.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+bash <(curl -L "https://raw.githubusercontent.com/joelmccracken/ws/${WS_VERSION}/ws_install.sh")
+
+if [ "$RUNNER_OS" == "macOS" ]; then
+    CI_WS_NAME=ci-macos
+else
+    CI_WS_NAME=ci-ubuntu
+fi
+
+$HOME/.local/share/ws/ws bootstrap -n "$CI_WS_NAME" \
+    --initial-config-repo 'https://github.com/joelmccracken/dotfiles.git' \
+    --initial-config-repo-ref "$DOTFILES_SHA"
