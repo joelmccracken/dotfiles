330d6d4618a906f2d9b5cdde6886d3afb2b6918f
Joel McCracken <mccracken.joel@gmail.com>
2025-04-05 23:07:19 -0400

    flip version variable roles

--- test/ci.sh
+++ test/ci.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-
-bash <(curl -L "https://raw.githubusercontent.com/joelmccracken/ws/${WS_PRE_BOOTSTRAP_VERSION}/ws_install.sh")
+bash <(curl -L "https://raw.githubusercontent.com/joelmccracken/ws/${WS_VERSION}/ws_install.sh")
 
 if [ "$RUNNER_OS" == "macOS" ]; then
     CI_WS_NAME=ci-macos
