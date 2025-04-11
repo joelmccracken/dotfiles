2f8860867b8c9e09738f0b477ee014c98082613a
Joel McCracken <mccracken.joel@gmail.com>
2025-04-11 12:29:58 -0400

    WIP nix home manager integraion

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,35 +3,30 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.11-darwin";
-    home-manager = {
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.11-darwin";
+    nixpkgs-linux.url = "github:nixos/nixpkgs/nixos-24.11";
+    home-manager-darwin = {
       url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
+    home-manager-linux = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs-linux";
+    };
+
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs-linux, nixpkgs-darwin, home-manager-linux, home-manager-darwin, ... }:
     let
-      home-config =
+
+      home-config-mod =
         { home, username }:
           { config, pkgs, ... }:
             {
-              # Home Manager needs a bit of information about you and the paths it should
-              # manage.
               home.username = username;
               home.homeDirectory = home;
 
-              # This value determines the Home Manager release that your configuration is
-              # compatible with. This helps avoid breakage when a new Home Manager release
-              # introduces backwards incompatible changes.
-              #
-              # You should not change this value, even if you update Home Manager. If you do
-              # want to update the value, then make sure to first check the Home Manager
-              # release notes.
-              home.stateVersion = "24.11"; # Please read the comment before changing.
-
-              # The home.packages option allows you to install Nix packages into your
-              # environment.
+              home.stateVersion = "24.11";
               home.packages = [
                 pkgs.git
                 pkgs.ripgrep
@@ -53,20 +48,7 @@
                 pkgs.racket
               ];
 
-              # Home Manager is pretty good at managing dotfiles. The primary way to manage
-              # plain files is through 'home.file'.
-              home.file = {
-                # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-                # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-                # # symlink to the Nix store copy.
-                # ".screenrc".source = dotfiles/screenrc;
-
-                # # You can also set the file content immediately.
-                # ".gradle/gradle.properties".text = ''
-                #   org.gradle.console=verbose
-                #   org.gradle.daemon.idletimeout=3600000
-                # '';
-              };
+              home.file = {};
 
               home.sessionPath = [
                 "~/.nix-profile/bin/"
@@ -77,80 +59,158 @@
                 extraPackages = epkgs: [ epkgs.vterm ];
               };
 
-              # Home Manager can also manage your environment variables through
-              # 'home.sessionVariables'. These will be explicitly sourced when using a
-              # shell provided by Home Manager. If you don't want to manage your shell
-              # through Home Manager then you have to manually source 'hm-session-vars.sh'
-              # located at either
-              #
-              #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-              #
-              # or
-              #
-              #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-              #
-              # or
-              #
-              #  /etc/profiles/per-user/joel.mccracken/etc/profile.d/hm-session-vars.sh
-              #
-              home.sessionVariables = {
-                # EDITOR = "emacs";
-              };
+              home.sessionVariables = {};
 
-              # Let Home Manager install and manage itself.
               programs.home-manager.enable = true;
             };
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."angrist"."joel.mccracken" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [
-          (home-config {
-            home="/Users/joel.mccraken";
-            username="joel.mccracken";
-          })
-        ];
+      home-config = settings@{nixpkgs, system, home, username, home-manager, ...}:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          home-config-mod =
+              { config, pkgs, ... }:
+                {
+                  home.username = username;
+                  home.homeDirectory = home;
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+                  home.stateVersion = "24.11";
+                  home.packages = [
+                    pkgs.git
+                    pkgs.ripgrep
+                    pkgs.jq
+                    # it is broken because of course it is
+                    # everything about nix being stable and repeatable is a lie
+                    # pkgs.jl
+                    pkgs.fd
+                    pkgs.ispell
+                    # it is broken because of course it is
+                    # everything about nix is an lie
+                    # pkgs.bitwarden-cli
+                    pkgs.direnv
+                    pkgs.mr  # myrepos https://myrepos.branchable.com/install/
+                    pkgs.graphviz
+                    pkgs.cmake
+                    pkgs.coreutils
+                    pkgs.wget
+                    pkgs.racket
+                  ];
+
+                  home.file = {};
+
+                  home.sessionPath = [
+                    "~/.nix-profile/bin/"
+                  ];
+
+                  programs.emacs = {
+                    enable = true;
+                    extraPackages = epkgs: [ epkgs.vterm ];
+                  };
+
+                  home.sessionVariables = {};
+
+                  programs.home-manager.enable = true;
+                };
+        in
+          home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [home-config-mod];
+          };
+
+      macConfig = settings:
+        {
+          homeConfigurations.${settings.hostname}.${settings.user} = home-config (
+            settings // { home-manager = home-manager-darwin; nixpkgs = nixpkgs-darwin; }
+          );
+        };
+
+      linuxConfig = settings:
+        {
+          homeConfigurations.${settings.ws-name}.${settings.user} = home-config (
+            settings // { home-manager = home-manager-linux;  nixpkgs = nixpkgs-linux; }
+          );
+        };
+
+      mergeDefs = m1: m2: {
+        # darwinConfigurations = (m1.darwinConfigurations or {}) // (m2.darwinConfigurations or {});
+        homeConfigurations = (m1.homeConfigurations or {}) // (m2.homeConfigurations or {});
       };
 
-      homeConfigurations."ci_macos"."runner" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      machineDefs = machines: builtins.foldl' mergeDefs {} machines;
+    in
+      machineDefs [
+        (macConfig {
+          user = "joel.mccracken"; ws-name = "angrist"; system = "aarch64-darwin"; home = "/Users/joel.mccraken";
+        })
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [
-          (home-config {
-            home="/Users/runner";
-            username="runner";
-          })
-        ];
+        (macConfig {
+          user = "joel"; ws-name = "aeglos"; system = "x86_64-darwin"; home = "/Users/joel";
+        })
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
+        (linuxConfig {
+          user = "joel"; ws-name = "belthronding"; system = "x86_64-linux"; home = "/home/joel";
+        })
 
-      homeConfigurations."ci_ubuntu"."runner" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        (macConfig {
+          user = "runner"; ws-name = "ci-macos"; system = "x86_64-darwin"; home = "/Users/runner";
+        })
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [
-          (home-config {
-            home="/home/runner";
-            username="runner";
-          })
-        ];
+        (linuxConfig {
+          user = "runner"; ws-name = "ci-ubuntu"; system = "x86_64-linux"; home = "/home/runner";
+        })
+      ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-    };
+
+
+
+    #   {
+    #   homeConfigurations."angrist"."joel.mccracken" = home-manager.lib.homeManagerConfiguration {
+    #     inherit pkgs;
+
+    #     # Specify your home configuration modules here, for example,
+    #     # the path to your home.nix.
+    #     modules = [
+    #       (home-config {
+    #         home="/Users/joel.mccraken";
+    #         username="joel.mccracken";
+    #       })
+    #     ];
+
+    #     # Optionally use extraSpecialArgs
+    #     # to pass through arguments to home.nix
+    #   };
+
+    #   homeConfigurations."ci_macos"."runner" = home-manager.lib.homeManagerConfiguration {
+    #     inherit pkgs;
+
+    #     # Specify your home configuration modules here, for example,
+    #     # the path to your home.nix.
+    #     modules = [
+    #       (home-config {
+    #         home="/Users/runner";
+    #         username="runner";
+    #       })
+    #     ];
+
+    #     # Optionally use extraSpecialArgs
+    #     # to pass through arguments to home.nix
+    #   };
+
+    #   homeConfigurations."ci_ubuntu"."runner" = home-manager.lib.homeManagerConfiguration {
+    #     inherit pkgs;
+
+    #     # Specify your home configuration modules here, for example,
+    #     # the path to your home.nix.
+    #     modules = [
+    #       (home-config {
+    #         home="/home/runner";
+    #         username="runner";
+    #       })
+    #     ];
+
+    #     # Optionally use extraSpecialArgs
+    #     # to pass through arguments to home.nix
+    #   };
+    # };
 }
 
 # nix run -v -L ".#homeConfigurations.\"angrist\".\"joel.mccracken\".activationPackage" --show-trace
