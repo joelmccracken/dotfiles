35810f242a7ddd4e3f6eb3bee34e8c8e726c01d5
Joel McCracken <mccracken.joel@gmail.com>
2025-05-03 17:31:43 -0400

    shuffle machine name settings

--- settings.ci_macos.sh
+++ settings.ci_macos.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 export WS_NAME=ci_macos
-export WS_PROP_NIX_HOME_MANAGER_FLAKE_OUTPUT='.#homeConfigurations."runner@ci_macos".activationPackage';
+export WS_PROP_NIX_HOME_MANAGER_FLAKE_OUTPUT=".#homeConfigurations.\"runner@$WS_NAME\".activationPackage";
